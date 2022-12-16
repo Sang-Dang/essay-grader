bc5d2bef666f2cbae26b9517c2164855a7f52c31
Sang Dang <sangdase170287@fpt.edu.vn>
2022-12-16 22:16:44 +0700

    task-achievement beginning
    error in script.js file. haven't finished coding in tab switching on switchstate() function

--- js/script.js
+++ js/script.js
@@ -4,6 +4,34 @@ function switchstate(obj) {
         for(var i = 0; i < all.length; i++) {
             all[i].classList.remove("active");
         }
+        var contents = document.getElementsByClassName("content");
+        for(var i = 0; i < contents.length; i++) {
+            contents[i].style.display = "none";
+        }
         obj.classList.add("active");
+        console.log(obj.classList[0]);
+        document.getElementsByClassName(obj.classList[0]).style.display = "block";
+    }
+}
+
+function clickRadioButton(obj) {
+    var currentClass = obj.classList[0];
+    var all = document.getElementsByClassName(currentClass);
+    for(var i = 0; i < all.length; i++) {
+        all[i].setAttribute("data-toggle", "false");
+        all[i].style.backgroundColor = "lightgray";
+    }
+    obj.setAttribute("data-toggle", "true");
+    obj.style.backgroundColor = "red";
+}
+
+function clickCheckButton(obj) {
+    if(obj.getAttribute("data-toggle") == "true") {
+        obj.setAttribute("data-toggle", "false");
+        obj.style.backgroundColor = "lightgray";
+    }
+    else {
+        obj.setAttribute("data-toggle", "true");
+        obj.style.backgroundColor = "red";
     }
 }
